1a6d224305037789048d168318b28a922db2adca
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-06-16 23:37:20 +0300

    50% done

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ const colors = [
   "#795548",
 ];
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector("button[data-action=start]"),
+  stopBtn: document.querySelector("button[data-action=stop]"),
+};
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+  setColor(colors);
+}
+
+function onStopBtnClick() {
+  refs.body.style.backgroundColor = "black";
+
+function setColor(colors) {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length)];
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
